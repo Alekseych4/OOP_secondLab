b0909bfe5d6c1ab5e98ec27444000176fdbd9a37
Alekseych4 <alekseych@list.ru>
2019-11-21 12:59:18 +0300

    Need to add angle for the Line

--- src/models/Bicycle.java
+++ src/models/Bicycle.java
@@ -1,5 +1,10 @@
 package models;
 
 public class Bicycle {
+    public static final String NAME = "велосипед";
 
+    private Point center;
+    private boolean visibility;
+    private Line steering, skeleton, seat, pole;
+    private Ring wheel;
 }
